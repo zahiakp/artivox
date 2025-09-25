2d878bd81b0992ae7b06391129ec6d0c7a496e42
zahiakp <121743451+zahiakp@users.noreply.github.com>
2025-09-25 16:17:59 +0530

    branding

---
+++ app/data/branding.ts
@@ -0,0 +1,11 @@
+export const brandName = "Avanzia"
+export const brandLogo = "/avanzia-logo.png"
+export const categoryMap: Record<string, string> = {
+  premier : "Premier",
+  junior: "Junior",
+  senior: "Senior",
+  // general: "General",
+};
+export const DocHeader = ""
+export const hexColor = "#7C3AED"
+
