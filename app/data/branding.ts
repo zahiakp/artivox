c53c4a17710c5b8753c3586c3db7e18f18687b11
zahiakp <121743451+zahiakp@users.noreply.github.com>
2025-11-18 20:36:25 +0530

    hss to hs

--- app/data/branding.ts
+++ app/data/branding.ts
@@ -1,7 +1,7 @@
 export const brandName = "Propicio"
 export const brandLogo = "/qadisiya-logo.png"
 export const categoryMap: Record<string, string> = {
-  hss : "HSS",
+  hs : "HS",
 };
 export const DocHeader = "/doc.png"
 export const DocHeaderSec = "/doc2.png"  
